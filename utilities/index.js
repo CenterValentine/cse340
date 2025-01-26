2c336e65ccbebdcdd4caa98770f8af3f983266f3
CenterValentine <david@doxy.me>
2025-01-25 17:19:28 -0700

    w03 css updates

--- utilities/index.js
+++ utilities/index.js
@@ -36,23 +36,41 @@ Util.buildClassificationGrid = async function(data){
     if(data.length > 0){
       grid = '<ul id="inv-display">'
       data.forEach(vehicle => { 
-        grid += '<li>'
-        grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-        + 'details"><img src="' + vehicle.inv_thumbnail 
-        +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-        +' on CSE Motors" /></a>'
-        grid += '<div class="namePrice">'
-        grid += '<hr />'
-        grid += '<h2>'
-        grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-        grid += '</h2>'
-        grid += '<span>$' 
-        + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-        grid += '</div>'
-        grid += '</li>'
+        grid += `<li><div>
+        <div class="class_lists__image">
+        <a href="/inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">
+        <img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors" />
+        </a>
+        </div>
+        <hr>
+        <div class="namePrice">
+          <h2> <a href="/inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">
+          ${vehicle.inv_make} ${vehicle.inv_model}</a></h2>
+          <div>
+          <span>$${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</span>|<span>${vehicle.inv_miles.toLocaleString()}</span>
+          </div>
+        </div>
+        </div>
+        </li>`
+
+        
+        // grid += '<li>'
+        // grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+        // + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+        // + 'details"><img src="' + vehicle.inv_thumbnail 
+        // +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+        // +' on CSE Motors" /></a>'
+        // grid += '<div class="namePrice">'
+        // grid += '<h2>'
+        // grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+        // + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+        // + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+        // grid += '</h2>'
+        // grid += '<span>$' 
+        // + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+        // grid += '</div>'
+        // grid += '<hr />'
+        // grid += '</li>'
       })
       grid += '</ul>'
     } else { 
@@ -78,19 +96,21 @@ Util.buildInventoryGrid = async function(data){
             break
         case 1:
             // grid = '<div><h1>' + vehicle.inv_model + ' ' + vehicle.inv_make + '</h1><div class="inv__details">'+ vehicle.inv_model + ' ' + vehicle.inv_make + '</div></div>'
-            grid = `<div>
-            <div class="inv__image">
-                <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_color} ${vehicle.inv_make} ${vehicle.inv_model}>
-            </div>
-            <div class="inv__details"> 
+            grid = `
+            <div id="inv_page__detail">
+              <div class="inv__image">
+                <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_color} ${vehicle.inv_make} ${vehicle.inv_model}">
+              </div>
+              <div class="inv__details"> 
                 <h2> ${vehicle.inv_model} ${vehicle.inv_make} details</h2>
-                <div class="inv__details-content">
-                Price: ${vehicle.inv_price}
-                Miles: ${vehicle.inv_miles}
-                Color: ${vehicle.inv_color}
-                Description: ${vehicle.inv_description}
-                </div>
-            </div>
+
+                <ul class="inv__details-content">
+                  <li><span>Price:</span> <span>$ ${vehicle.inv_price.toLocaleString()}</span></li>
+                  <li><span>Miles:</span> <span>${vehicle.inv_miles.toLocaleString()}</span></li>
+                 <li><span>Color:</span> <span>${vehicle.inv_color}</span></li>
+                 <li class="inv__details-content_description" ><span>Description: </span><span>${vehicle.inv_description}</span></li>
+</ul>
+              </div>
             </div>`
             break
         default:
